7edaad07a9acf232cc68ec5bdcb4561124e0308f
Andrew Duckworth <a.duckworth96@live.com>
2023-03-03 10:20:13 +1300

    added some routes

--- server/server.js
+++ server/server.js
@@ -16,10 +16,26 @@ server.set('views', __dirname + '/views')
 // Your routes/router(s) should go here
 
 server.get('/', (req, res) => {
-    dataObj = {
-        title:'the Wisdom of Zoltash'
-    }
-    res.render('home', dataObj)
+  dataObj = {
+    title: 'the Wisdom of Zoltash',
+  }
+  res.render('home', dataObj)
+})
+
+// Q1 route
+server.get('/q1', (req, res) => {
+  dataObj = {
+    title: 'Question One',
+  }
+  res.render('home', dataObj) //<----- Change to 'q1'
+})
+
+// Fortune route
+server.get('/fortune', (req, res) => {
+  dataObj = {
+    title: 'Here is your fortune',
+  }
+  res.render('home', dataObj) //<----- Change to 'fortune'
 })
 
 module.exports = server
