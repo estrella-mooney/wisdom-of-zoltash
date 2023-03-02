77d495dd0231a470df50a27b6913b65645772e49
Jennifer Harper <jennnnifer.harper@gmail.com>
2023-03-03 10:58:27 +1300

    Button to home page and tidy css

--- server/server.js
+++ server/server.js
@@ -28,15 +28,7 @@ server.get('/q1', (req, res) => {
   dataObj = {
     title: 'Question One',
   }
-  res.render('q1', dataObj) //<----- Change to 'q1'
-})
-
-// Server.get for the Question two
-server.get('/q2', (request, response) => {
-  dataObj = {
-    title: 'the Wisdom of Zoltash',
-  }
-  response.render('q2', dataObj) //Make sure to change this to Question Two when we have page
+  res.render('home', dataObj) //<----- Change to 'q1'
 })
 
 // Fortune route
@@ -46,5 +38,14 @@ server.get('/fortune', (req, res) => {
   }
   res.render('home', dataObj) //<----- Change to 'fortune'
 })
+// Server.get for the Question two
+server.get('/q2', (request, response) => {
+  dataObj = {
+    title: 'the Wisdom of Zoltash',
+  }
+  response.render('home', dataObj) //Make sure to change this to Question Two when we have page
+})
+
+
 
 module.exports = server
