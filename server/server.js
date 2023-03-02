f1ee9cb430446bed90880f70ab06084af28937e7
Estrella Mooney <estrellamooney96@gmail.com>
2023-03-03 10:18:59 +1300

    Route for Question Two completed

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const res = require('express/lib/response')
 
 const server = express()
 
@@ -16,10 +17,18 @@ server.set('views', __dirname + '/views')
 // Your routes/router(s) should go here
 
 server.get('/', (req, res) => {
-    dataObj = {
-        title:'the Wisdom of Zoltash'
-    }
-    res.render('home', dataObj)
+  dataObj = {
+    title: 'the Wisdom of Zoltash',
+  }
+  res.render('home', dataObj)
+})
+
+// Server.get for the Question two
+server.get('/q2', (request, response) => {
+  dataObj = {
+    title: 'the Wisdom of Zoltash',
+  }
+  response.render('home', dataObj) //Make sure to change this to Question Two when we have page
 })
 
 module.exports = server
