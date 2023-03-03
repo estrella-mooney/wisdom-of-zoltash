15ac511aa41c74d77160e5ccde02069f2723f1ab
Jennifer Harper <jennnnifer.harper@gmail.com>
2023-03-03 15:05:32 +1300

    fortune page gets ramdom fortune, doesnt include form data

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const fs = require('node:fs/promises')
 const express = require('express')
 const hbs = require('express-handlebars')
 const res = require('express/lib/response')
@@ -29,20 +30,38 @@ server.get('/', (req, res) => {
 
 // Q1 route
 server.get('/q1', (req, res) => {
-  dataObj = {
+  let dataObj = {
     title: 'Question One',
   }
   res.render('q1', dataObj) //<----- Change to 'q1'
 })
 
+<<<<<<< HEAD
 // Server.get for the Question two
 server.get('/q2', (request, response) => {
   dataObj = {
     title: 'Question Two',
+=======
+
+
+server.post('/q1', (req, res) => {
+  const formStuff = req.body
+  const answerOne = formStuff.a1;
+  console.log(answerOne)
+  res.redirect('q1')
+})
+
+
+// Server.get for the Question two
+server.get('/q2', (request, response) => {
+  let dataObj = {
+    title: 'the Wisdom of Zoltash',
+>>>>>>> 1ad1dec (fortune page gets ramdom fortune, doesnt include form data)
   }
   response.render('q2', dataObj) //Make sure to change this to Question Two when we have page
 })
 
+<<<<<<< HEAD
 //Estrella CHANGES ------->
 server.post('/q2', (req, res) => {
   console.log(req.body)
@@ -51,11 +70,23 @@ server.post('/q2', (req, res) => {
 })
 
 // Fortune route
+=======
+
+>>>>>>> 1ad1dec (fortune page gets ramdom fortune, doesnt include form data)
 server.get('/fortune', (req, res) => {
-  dataObj = {
-    title: 'Here is your fortune',
-  }
-  res.render('fortune', dataObj) //<----- Change to 'fortune'
+  fs.readFile(__dirname + '/data/data.json', 'utf-8')
+  .then((answer) => {
+    let thing = JSON.parse(answer)
+    let randomIndex = Math.floor(Math.random() * thing.fortunes.length)
+    let fortuneAnswer = {
+      title: 'Here is your fortune',
+      fortune: thing.fortunes[randomIndex] 
+    }
+    res.render('fortune', fortuneAnswer)
+  })
+    .catch((err) => {
+      res.status(500).send(err.message)
+    })
 })
 
 module.exports = server
